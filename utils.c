b4d010d8f4bacff1b3cfa7bebe392db7c809f2e6
Mulling <lucas_mulling@hotmail.com>
2021-08-24 17:33:30 -0300

    filter packets by id
    added ICMP_TIME_EXCEEDED and ICMP_UNREACH

--- utils.c
+++ utils.c
@@ -7,6 +7,8 @@
 #define OUTFMT "%d bytes from %s icmp_seq=%d ttl=%u time=%.1fms"
 #define TMOFMT "timeout(%ds) for icmp_seq=%d\n"
 #define DUPFMT "\x1b[5m DUP!\x1b[0m\n"
+#define EXCFMT "time to live exceeded with ttl= %d\n"
+#define UNRFMT "destination host unreachable!\n"
 #define RESFMT0 "\nRESULTS:\n"
 #define RESFMT1 "sent=%lu received=%lu (%.1f%% packet loss.)\n"
 #define RESFMT2 "min=%.1fms rtt=%.1fms max=%.1fms\n"
